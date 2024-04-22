43cfe63eae78bc4e551d5305997d71a5f63adaff
Shubhansh0311 <shubhanshagrawal0311@gmail.com>
2024-04-22 16:21:31 +0530

    frontend-5

--- settingapp/src/url.js
+++ settingapp/src/url.js
@@ -1,3 +1,3 @@
-const url=process.env.base_url||"https://settingappbackend.vercel.app";
+const url=process.env.base_url;
 export const bluetoothGetStatus = `${url}/bluetooth/status`;
 export const bluetoothPostStatus = `${process.env.base_url}/bluetooth/toggle`;
