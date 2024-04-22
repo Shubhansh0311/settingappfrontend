5f5725beb76e56e43d75adba316f2fbd7dbfc6dc
Shubhansh0311 <shubhanshagrawal0311@gmail.com>
2024-04-22 16:56:14 +0530

    frontend-15

--- settingapp/src/user.js
+++ settingapp/src/user.js
@@ -1,16 +1,16 @@
 
-// Get the user agent string
-var userAgent = navigator.userAgent
-if (userAgent.match(/Android/i)) {
-}
-// Check if the user agent contains specific keywords to identify the device
-else if (userAgent.match(/iPhone|iPad|iPod/i)) {
-  // Code for iOS devices
-  app3.style.display = 'none'
-  app4.style.display = 'none'
-}
-// Code for Windows devices
-else {
-  mainDiv.style.alignItems = 'center'
-  mainDiv.style.justifyContent = 'center'
-}
+// // Get the user agent string
+// var userAgent = navigator.userAgent
+// if (userAgent.match(/Android/i)) {
+// }
+// // Check if the user agent contains specific keywords to identify the device
+// else if (userAgent.match(/iPhone|iPad|iPod/i)) {
+//   // Code for iOS devices
+//   app3.style.display = 'none'
+//   app4.style.display = 'none'
+// }
+// // Code for Windows devices
+// else {
+//   mainDiv.style.alignItems = 'center'
+//   mainDiv.style.justifyContent = 'center'
+// }
