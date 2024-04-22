0c3c62ed49e12814f999a0f6005827896a7f2fad
Shubhansh0311 <shubhanshagrawal0311@gmail.com>
2024-04-22 14:50:07 +0530

    bluetooth

--- settingapp/src/url.js
+++ settingapp/src/url.js
@@ -1,2 +1,3 @@
-export const bluetoothGetStatus = `${process.env.base_url}/bluetooth/status`;
-export const bluetoothPostStatus=`${process.env.base_url}/toggle`
+
+export const bluetoothGetStatus = `${process.env.base_url}bluetooth/status`;
+export const bluetoothPostStatus=`${process.env.base_url}bluetooth/toggle`
