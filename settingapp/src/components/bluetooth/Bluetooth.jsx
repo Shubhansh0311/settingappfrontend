c36817e64be3691094dc3bc4478053474a1da122
Shubhansh0311 <shubhanshagrawal0311@gmail.com>
2024-04-22 16:27:30 +0530

    frontend-7

--- settingapp/src/components/bluetooth/Bluetooth.jsx
+++ settingapp/src/components/bluetooth/Bluetooth.jsx
@@ -21,7 +21,7 @@ const Bluetooth = () => {
       try {
         const response = await axios.get(bluetoothGetStatus)
           .then((e) => {
-            console.log(e)})
+            console.log(e.data.json())})
         
           .catch((err) => {
             console.log(err);
